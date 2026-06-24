2d0883330e0bfc3d135d04989d420f8f4a10a73c
Shambhavi Mishra <shambhavim450@gmail.com>
2026-06-24 18:59:45 +0530

    Added fraud detection logic

--- app.py
+++ app.py
@@ -9,32 +9,31 @@ from flask import render_template
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+ return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
     text = data.get("text", "")
-    fraud_keywords = [
-    "cbI officer", "digital arrest", "account blocked",
-    "send otp", "verify bank", "urgent transfer"
-]
 
-if any(word.lower() in text.lower() for word in fraud_keywords):
-    return jsonify({
-        "input": text,
-        "prediction": "FRAUD / SCAM 🚨 (Rule-based detection)"
-    })
+    fraud_keywords = [
+        "cbi officer", "digital arrest", "account blocked",
+        "send otp", "verify bank", "urgent transfer"
+    ]
+
+    if any(word.lower() in text.lower() for word in fraud_keywords):
+        return jsonify({
+            "input": text,
+            "prediction": "FRAUD / SCAM 🚨 (Rule-based detection)"
+        })
 
     X = vectorizer.transform([text])
     prediction = model.predict(X)[0]
 
-  if prediction == 1:
-    result = "SAFE ✅"
-elif prediction == 0:
-    result = "SUSPICIOUS ⚠️"
-else:
-    result = "FRAUD / SCAM 🚨" 
+    if prediction == 1:
+        result = "SAFE ✅"
+    else:
+        result = "SUSPICIOUS ⚠️"
 
     return jsonify({
         "input": text,
@@ -42,4 +41,4 @@ else:
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+ app.run(debug=True)
